ff05556e0d47c69a02d6586da2b9c34a7e49e772
Vinicius Hansel <hanelvinicius@gmail.com>
2025-04-26 23:12:14 -0300

    ci(main.ts): if to validate dev enviroment

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,9 @@ import { AppModule } from './configuration/app.module';
 import { loadEnvFile } from 'node:process';
 
 async function bootstrap(): Promise<void> {
-  loadEnvFile();
+  if(process.env.NODE_ENV === 'dev') {
+    loadEnvFile();
+  }
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
 }
