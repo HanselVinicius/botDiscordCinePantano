a31830e057412abf5ad77a4d6019a8e4e49a662f
Vinicius Hansel <hanelvinicius@gmail.com>
2025-04-29 20:41:28 -0300

    ci(PRD): making changes so it reflect to PRD env

--- src/gateway/movie/repository/MovieRepositoryImpl.spec.ts
+++ src/gateway/movie/repository/MovieRepositoryImpl.spec.ts
@@ -6,7 +6,7 @@ describe('MovieRepositoryImpl', () => {
     it('should get movie list and return an list of movies', async () => {
         // arrange
         const request = {
-            getAuthenticated: jest.fn().mockResolvedValue([{} as Movie]),
+            get: jest.fn().mockResolvedValue([{} as Movie]),
         } as unknown as jest.Mocked<ApiRequestHandler>;
         const movieRepositoryImpl = new MovieRepositoryImpl(request);
         const options = { isSeen: true, limit: 10, page: 1 };
@@ -15,7 +15,7 @@ describe('MovieRepositoryImpl', () => {
         const result = await movieRepositoryImpl.getMovieList(options);
 
         // assert
-        expect(request.getAuthenticated).toHaveBeenCalledWith(`v1/movie?page=${options.page}&limit=${options.limit}`, process.env.INTEGRATION_TOKEN);
+        expect(request.get).toHaveBeenCalledWith(`v1/movie?page=${options.page}&limit=${options.limit}`);
         expect(result).toEqual([{} as Movie]);
     });
 });
--- src/gateway/movie/repository/MovieRepositoryImpl.ts
+++ src/gateway/movie/repository/MovieRepositoryImpl.ts
@@ -6,7 +6,7 @@ export class MovieRepositoryImpl implements MovieRepository{
     constructor(private readonly request:ApiRequestHandler) { }
 
     public async getMovieList(options?:{isSeen:Boolean,limit:number,page:number}): Promise<Movie[]> {
-        const result = await this.request.getAuthenticated<Movie[]>(`v1/movie?page=${options.page}&limit=${options.limit}`,process.env.INTEGRATION_TOKEN); 
+        const result = await this.request.get<Movie[]>(`v1/movie?page=${options.page}&limit=${options.limit}`); 
         return result;
     }
 }
